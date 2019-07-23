ce7dd87490e326b76a77d731150af308d6992233
pavel <minikspb@gmail.com>
2019-07-23 12:01:26 +0300

    db migration

--- app/state_machines/models.py
+++ app/state_machines/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 from django.contrib.postgres.fields import JSONField
 
+from transport.models import Endpoint
+
 
 class StateMachine(models.Model):
     id = models.CharField(max_length=512, primary_key=True)
     last_access = models.DateTimeField(auto_now=True)
     context = JSONField()
+    endpoint = models.ForeignKey(Endpoint, on_delete=models.CASCADE, related_name='state_machines', null=True)
