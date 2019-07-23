a6ea15a2c9b809c033f45ffebe4a2a93da0a9ec3
pavel <minikspb@gmail.com>
2019-07-23 16:39:38 +0300

    db migration

--- app/authentication/models.py
+++ app/authentication/models.py
@@ -1,12 +1,10 @@
+from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
 class AgentAccount(AbstractUser):
-    """
-    Users within the Django authentication system are represented by this
-    model.
 
-    Username and password are required. Other fields are optional.
-    """
+    agent_name = models.CharField(max_length=64, unique=True, null=True)
+
     class Meta(AbstractUser.Meta):
         swappable = 'AUTH_USER_MODEL'
