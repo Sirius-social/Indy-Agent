6476838dda56a23e9c087ec9361646e0b05c31f5
pavel <minikspb@gmail.com>
2019-07-23 10:54:31 +0300

    db migration

---
+++ app/transport/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+from authentication.models import AgentAccount
+
+
+class Endpoint(models.Model):
+    uid = models.CharField(max_length=2083, primary_key=True)
+    owner = models.ForeignKey(AgentAccount, related_name='endpoints', on_delete=models.CASCADE)
