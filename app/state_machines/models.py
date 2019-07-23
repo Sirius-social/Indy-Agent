6e61362a768273cdf4f73d47c2121074ee85e694
pavel <minikspb@gmail.com>
2019-07-23 12:15:33 +0300

    db migration

--- app/state_machines/models.py
+++ app/state_machines/models.py
@@ -8,4 +8,3 @@ class StateMachine(models.Model):
     id = models.CharField(max_length=512, primary_key=True)
     last_access = models.DateTimeField(auto_now=True)
     context = JSONField()
-    endpoint = models.ForeignKey(Endpoint, on_delete=models.CASCADE, related_name='state_machines', null=True)
