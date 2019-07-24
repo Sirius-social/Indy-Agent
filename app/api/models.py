3738c8dd1f0c3edf1a272de7c674750ad2f4598b
pavel <minikspb@gmail.com>
2019-07-24 20:49:54 +0300

    db migration

--- app/api/models.py
+++ app/api/models.py
@@ -4,12 +4,6 @@ from authentication.models import AgentAccount
 
 
 class Wallet(models.Model):
-
-    STATUSES = (
-        ('opened', 'Opened'),
-        ('closed', 'Closed'),
-    )
-
     owner = models.ForeignKey(AgentAccount, on_delete=models.CASCADE, related_name='wallets')
     name = models.CharField(max_length=512)
-    status = models.CharField(max_length=64, choices=STATUSES, null=True)
+
