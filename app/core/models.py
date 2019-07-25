92ac8f51a1404e6e5eea75706c466c9cfc1edc40
pavel <minikspb@gmail.com>
2019-07-26 02:19:55 +0300

    db migration

--- app/core/models.py
+++ app/core/models.py
@@ -1,1 +1,6 @@
 from django.db import models
+
+
+class StartedStateMachine(models.Model):
+    machine_id = models.CharField(max_length=512, primary_key=True)
+    machine_class_name = models.CharField(max_length=512)
