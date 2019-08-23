c2649c14c809dcea09e18be6b86fce0eadf04596
pavel <minikspb@gmail.com>
2019-08-23 18:57:43 +0300

    db migration

--- app/transport/models.py
+++ app/transport/models.py
@@ -9,3 +9,4 @@ class Endpoint(models.Model):
     uid = models.CharField(max_length=2083, primary_key=True)
     owner = models.ForeignKey(AgentAccount, related_name='endpoints', on_delete=models.CASCADE)
     wallet = models.ForeignKey(Wallet, related_name='endpoints', on_delete=models.CASCADE, null=True)
+    url = models.URLField(null=True)
