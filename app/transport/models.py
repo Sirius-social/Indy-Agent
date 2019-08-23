1f1e737e1ea46c8d6a7bd71f4de77b3e00ff8095
pavel <minikspb@gmail.com>
2019-08-23 23:07:32 +0300

    db migration

--- app/transport/models.py
+++ app/transport/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.postgres.fields import JSONField
 
 from authentication.models import AgentAccount
 from api.models import Wallet
@@ -10,3 +10,9 @@ class Endpoint(models.Model):
     owner = models.ForeignKey(AgentAccount, related_name='endpoints', on_delete=models.CASCADE)
     wallet = models.ForeignKey(Wallet, related_name='endpoints', on_delete=models.CASCADE, null=True)
     url = models.URLField(null=True)
+
+
+class Invitation(models.Model):
+    endpoint = models.ForeignKey(Endpoint, on_delete=models.CASCADE, related_name='invitations')
+    invitation_string = models.CharField(max_length=1024)
+    invite_message = JSONField()
