65dce1705e53197aed035e1f0a89fad81e2bf006
pavel <minikspb@gmail.com>
2019-08-23 23:07:43 +0300

    refactoring

--- app/state_machines/models.py
+++ app/state_machines/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.postgres.fields import JSONField
-from django.dispatch import receiver
 
 from transport.models import Endpoint
 
@@ -10,9 +9,3 @@ class StateMachine(models.Model):
     last_access = models.DateTimeField(auto_now=True)
     context = JSONField()
     endpoint_uid = models.CharField(max_length=2083, db_index=True, null=True)
-
-
-@receiver(models.signals.post_delete, sender=Endpoint)
-def auto_clean_state_machines(sender, instance, **kwargs):
-    StateMachine.objects.filter(endpoint_uid=instance.uid).all().delete()
-    pass
