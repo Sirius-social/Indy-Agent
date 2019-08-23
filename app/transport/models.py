134188586991cb690b1ed116cd5a676ab10da679
pavel <minikspb@gmail.com>
2019-08-23 17:40:05 +0300

    db migration

--- app/transport/models.py
+++ app/transport/models.py
@@ -2,8 +2,10 @@ from django.db import models
 
 
 from authentication.models import AgentAccount
+from api.models import Wallet
 
 
 class Endpoint(models.Model):
     uid = models.CharField(max_length=2083, primary_key=True)
     owner = models.ForeignKey(AgentAccount, related_name='endpoints', on_delete=models.CASCADE)
+    wallet = models.ForeignKey(Wallet, related_name='endpoints', on_delete=models.CASCADE, null=True)
