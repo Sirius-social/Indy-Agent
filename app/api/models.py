7e4a4424275e6d5f26e16f3d4d50f628f7d70b74
pavel <minikspb@gmail.com>
2019-08-26 11:59:37 +0300

    db migration

--- app/api/models.py
+++ app/api/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.dispatch import receiver
 
 from authentication.models import AgentAccount
 
@@ -7,9 +6,3 @@ from authentication.models import AgentAccount
 class Wallet(models.Model):
     uid = models.CharField(max_length=512, unique=True)
     owner = models.ForeignKey(AgentAccount, on_delete=models.CASCADE, null=True)
-
-
-class Connection(models.Model):
-    value = models.CharField(max_length=128)
-    owner = models.ForeignKey(AgentAccount, on_delete=models.CASCADE)
-    wallet = models.ForeignKey(Wallet, on_delete=models.SET_NULL, null=True)
