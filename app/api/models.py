9ba5b1a0a217893761a86d487b613c8efc58ee67
pavel <minikspb@gmail.com>
2019-08-26 16:37:33 +0300

    db migration

--- app/api/models.py
+++ app/api/models.py
@@ -5,4 +5,4 @@ from authentication.models import AgentAccount
 
 class Wallet(models.Model):
     uid = models.CharField(max_length=512, unique=True)
-    owner = models.ForeignKey(AgentAccount, on_delete=models.CASCADE, null=True)
+    owner = models.ForeignKey(AgentAccount, on_delete=models.CASCADE, null=True, related_name='wallets')
