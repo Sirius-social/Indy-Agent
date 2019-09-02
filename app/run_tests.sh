bb7eeb342930264bf6548d06026764fc2b6edf7a
pavel <minikspb@gmail.com>
2019-09-02 17:22:13 +0300

    refactoring

--- app/run_tests.sh
+++ app/run_tests.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 cd /app
-pytest
+
+echo "Running PyTesta"
+pytest core/tests/pytest_wallets.py
+pytest core/tests/pytest_reqresp.py
+pytest core/tests/pytest_channels.py
+pytest core/tests/pytest_aries_0160_connection_protocol.py
+pytest core/tests/pytest_aries_0023_did_exchange.py
+pytest state_machines/tests/pytest_base_state_machine.py
+
+echo "Run Django Tests"
 python manage.py test --noinput
