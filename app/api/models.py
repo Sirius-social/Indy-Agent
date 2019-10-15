63c1df4ff9132b6d3a55847460dcacd1f55c304d
pavel <minikspb@gmail.com>
2019-10-15 18:48:56 +0300

    db migration

--- app/api/models.py
+++ app/api/models.py
@@ -6,3 +6,7 @@ from authentication.models import AgentAccount
 class Wallet(models.Model):
     uid = models.CharField(max_length=512, unique=True)
     owner = models.ForeignKey(AgentAccount, on_delete=models.CASCADE, null=True, related_name='wallets')
+
+
+class SchemaDefinition(models.Model):
+    json = models.CharField(max_length=2056)
